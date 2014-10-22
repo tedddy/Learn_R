c049f3a2d22c90bec25b268f2f56420c9581ce46
tedddy <dule1-g@qq.com>
2014-10-22 14:54:07 +0800

    getURL example 1

--- cos_name_XiaoNan.R
+++ cos_name_XiaoNan.R
@@ -1,17 +1,17 @@
-#http://hq.sinajs.cn/list=sh601318
+# http://cos.name/; http://cos.name/cn/topic/17816
 library(RCurl)
 library(RODBC)
 library(bitops)
 library(XML)##这些包的安装比较麻烦，尤其是XML
 library(stringr)
 library(plyr)
-###此处专用于解决XML中文乱码问题###
+
 myHttpheader<- c(
   "User-Agent"="Mozilla/5.0(Windows; U; Windows NT 5.1; zh-CN; rv:1.9.1.6)",
   "Accept"="text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-  "Accept-Language"="en-us, zh_CN",
+  "Accept-Language"="en-us",
   "Connection"="keep-alive",
   "Accept-Charset"="GB2312,utf-8;q=0.7,*;q=0.7"
 )
-###以上这段不需要改
-rt_601318 <- getURL("http://hq.sinajs.cn/list=sh601318",httpheader=myHttpheader)
+
+temp<- getURL("http://cos.name/",httpheader=myHttpheader)
