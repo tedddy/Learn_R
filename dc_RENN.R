785a4915f67f9e66259e0021e576e76d5dc2534b
tedddy <dule1-g@qq.com>
2014-10-26 21:44:57 +0800

    fetch data of RENN DonchianChannel for RENN

--- dc_RENN.R
+++ dc_RENN.R
@@ -1,4 +1,11 @@
 library(TTR)
+library(zoo)
+library(xts)
 RENN <- getYahooData("RENN",20140101,20141001)
 temp_RENN <- RENN[,c("High","Low")]
 dc_RENN <- DonchianChannel(temp_RENN)
+
+#? extract_dc_RENN <- dc_RENN[[1]]; dc_RENN
+
+extract_dc_RENN <- dc_RENN[[2]]
+
