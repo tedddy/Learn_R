d84130975a3f682511e5b00311663679f1e4b10b
tedddy <dule1-g@qq.com>
2014-10-29 15:38:20 +0800

    intro-8

--- learn/intro-R/intro-8.r
+++ learn/intro-R/intro-8.r
@@ -1,5 +1,39 @@
+# R as a set of statistical tables
+
 ?beta()
 d_beta()
 ?norm()
 pnorm(0)
 ?pnorm()
+2*pt(-2.43,df=13)
+
+qf(0.01,2,7,lower.tail=FALSE)
+
+# Examining the distribution of a set of data
+
+attach(faithful)
+
+summary(eruptions)
+
+faithful_temp <- faithful
+
+temp_faithful<- faithful
+
+fivenum(eruptions)
+
+stem(eruptions)
+
+?stem()
+
+hist(eruptions)
+
+hist(eruptions, seq(1.6, 5.2, 0.2), prob=TRUE)
+
+lines(density(eruptions, bw=0.1))
+
+rug(eruptions)
+
+lines(density(eruptions, bw="SJ"))
+
+rug(eruptions)
+
