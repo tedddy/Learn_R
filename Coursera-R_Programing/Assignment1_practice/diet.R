e5578e340a4b04ff72c1f8dce24d6eab424a22d6
tedddy <dule1-g@qq.com>
2014-11-22 19:27:54 +0800

    141122-4

---
+++ Coursera-R_Programing/Assignment1_practice/diet.R
@@ -0,0 +1,6 @@
+# Download and unzip data 
+dataset_url <- "http://s3.amazonaws.com/practice_assignment/diet_data.zip"
+download.file(dataset_url, "diet_data.zip")
+unzip("diet_data.zip", exdir = "diet_data")
+
+list.files("diet_data")
