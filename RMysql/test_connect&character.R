4c416fcaeb330d931967b029c7168c285d050862
tedddy <dule1-g@qq.com>
2015-08-02 10:19:25 +0800

    150802

---
+++ RMysql/test_connect&character.R
@@ -0,0 +1,10 @@
+require(RMySQL)
+dbh <- dbConnect(MySQL(), user="gxh", password="locoy", dbname="ying", host="192.168.137.172")
+dbListTables(dbh)
+dbSendQuery(dbh,'SET NAMES utf8')
+dbGetQuery(dbh, "SELECT * FROM ying.hk_w;")
+res=dbSendQuery(dbh, "SELECT * FROM ying.hk_w;")
+dat=fetch(res)
+dat
+
+#test
