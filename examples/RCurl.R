728795311443e930599f1e306122168331006710
tedddy <dule1-g@qq.com>
2016-03-09 13:01:18 +0800

    c

--- examples/RCurl.R
+++ examples/RCurl.R
@@ -10,8 +10,9 @@ myHttpheader <- c(
     "User-Agent"="Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36"
 )
 jpg <- getBinaryURL("http://img14.360buyimg.com/n1/g2/M04/07/00/rBEGEFAoV-IIAAAAAACLk5lmyBgAABXmwEq69gAAIur232.jpg",httpheader=myHttpheader)
-writeBin(jpg, con = "C:\\Users\\Xianghua\\Pictures\\RCurl\\123.jpg") #保存图片
-shell.exec(tf) #显示图片
+# writeBin(jpg, con = "C:\\Users\\Xianghua\\Pictures\\RCurl\\123.jpg") #保存图片
+writeBin(jpg, con = "/home/tony/lr/examples/picture/jd.jpg") #保存图片
+#shell.exec(tf) #显示图片
 
 library(RCurl)
 myHttpheader <- c(
@@ -25,5 +26,5 @@ myHttpheader <- c(
     "User-Agent"="Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36"
 )
 png <- getBinaryURL("http://push.shangdu.com/regcode.php?token=prrX73LtmN_63uEcKh0a2Z1opo1ZgdcBJ",httpheader=myHttpheader)
-writeBin(png, con = "C:/123.png") #保存图片
+writeBin(png, con = "/home/tony/lr/examples/picture/123.png") #保存图片
 shell.exec(tf) #显示图片
