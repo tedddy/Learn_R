2f1bc8f1804704c7c0af3f52a47fc343bca2b669
tedddy <dule1-g@qq.com>
2016-04-06 17:49:08 +0800

    c

--- index/i_macd_d.R
+++ index/i_macd_d.R
@@ -1,15 +1,8 @@
 require(quantmod)
 require(RMySQL)
 
-<<<<<<< HEAD
+
 source('~/lr/functions/sqlQuery_fun.R')
-
-source('~/lr/functions/i_macd_daily_fun.R')
-=======
-source('~/lr/index/functions/sqlQuery_fun.R')
-
-source('~/lr/index/functions/i_macd_daily_fun.R')
->>>>>>> 0f6f883da0901e80f28aaeb70a24a36f5a01172b
 
 
 # fun_i_macd_daily('399934',limit = '9999')
@@ -23,11 +16,5 @@ for (idi in i_FJ_info$idi) {
     fun_i_macd_daily(idi)
 }
 # run daily end
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 0f6f883da0901e80f28aaeb70a24a36f5a01172b
 
 # fun_i_macd_daily('399006')
